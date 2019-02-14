4ef9b1c3328b0a8565db5b9fdfb21114292f8e77
Dana Roberts <droberts@pixelglory.com>
2019-02-14 14:15:35 -0600

    remove uneccessary code

--- content/themes/farris/resources/views/partials/pagebuilder-text.blade.php
+++ content/themes/farris/resources/views/partials/pagebuilder-text.blade.php
@@ -1,16 +1,4 @@
-<section class="module-text module--previous-{!! $previous_module_style !!} section-container">
-  
-  @if( $previous_module_style == 'transparent')
-  <div class="module__cap-outer module__cap-outer--bottom">
-    <div class="container">
-      <div class="row">
-        <div class="col-10 offset-1">
-          <div class="module__capper module__capper--bottom"></div>
-        </div>
-      </div>
-    </div>
-  </div>
-  @endif
+<section class="module-text section-container">
   
   <div class="container">
     <div class="row">
