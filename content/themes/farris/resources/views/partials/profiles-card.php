412e5234d8c2dd938b2c0af4ed2e9d4c8515fa67
ozge <ozgecokyasar@gmail.com>
2019-02-20 21:45:36 -0800

    add profile card component for desktop and mobile

---
+++ content/themes/farris/resources/views/partials/profiles-card.php
@@ -0,0 +1,22 @@
+
+<div class="container">
+  <div class="profile__card">
+  <div class="row justify-content-start">
+
+        <div class="col-lg-12 col-md-12 col-5 padding-0">
+          <div class="profile__card-photo">
+            <img height="100%" width="100%" src="http://www.everlastingcelebrations.com/wp-content/uploads/2018/09/Top-Ganesh-Chaturthi-Messages-Images-Photos.jpg"/>
+          </div>
+        </div>
+
+        <div class="col-lg-12 col-md-12 col-7 padding-0">
+          <div class="profile__card-text">
+            <h6 class="green">Name Surname</h6>
+            <p class="small">Position, City</p>
+            <p class="small">namesurname@farris.com</p>
+          </div>
+        </div>
+
+    </div>
+  </div>
+</div>
