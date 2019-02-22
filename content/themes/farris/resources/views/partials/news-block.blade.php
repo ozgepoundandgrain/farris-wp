846b82df2a4993cfa92657390adafda0a0a1744b
ozge <ozgecokyasar@gmail.com>
2019-02-22 14:00:19 -0800

    add desktop news block

---
+++ content/themes/farris/resources/views/partials/news-block.blade.php
@@ -0,0 +1,17 @@
+<div class="container">
+  <div class="row news__card-container">
+    <div class="col-5">
+      <div class="news__card-image" style="background: red;">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxu3FrWCOi59a-Zems8rHMPaxxsKThUlRg6-ZOGe9fFgmMblQi" />
+      </div>
+    </div>
+
+    <div class="col-7 news__card-text">
+        <p class="small news__card-subheading">ARTICLE</p>
+        <h3>David Selley acts for Translink in the issuance of $400 million of 3.25% green bonds</h3>
+        <p class="small news__card-date">January 29, 2019  •  Joseph Smith</p>
+        <p>On September 21, 2018, the City of Vancouver issued $85 million of 3.10% green bonds due September 21, 2028.  This represented the City’s first green bond offering.</p>
+    </div>
+
+  </div>
+</div>
