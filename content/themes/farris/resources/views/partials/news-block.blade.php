34a1f79e23fe8d012e41da3d10ba9f4f833f691d
ozge <ozgecokyasar@gmail.com>
2019-02-22 14:21:08 -0800

    ammend news block

--- content/themes/farris/resources/views/partials/news-block.blade.php
+++ content/themes/farris/resources/views/partials/news-block.blade.php
@@ -1,12 +1,12 @@
 <div class="container">
   <div class="row news__card-container">
-    <div class="col-5">
+    <div class="col-lg-5 col-sm-12">
       <div class="news__card-image" style="background: red;">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxu3FrWCOi59a-Zems8rHMPaxxsKThUlRg6-ZOGe9fFgmMblQi" />
       </div>
     </div>
 
-    <div class="col-7 news__card-text">
+    <div class="col-lg-7 col-sm-12 news__card-text">
         <p class="small news__card-subheading">ARTICLE</p>
         <h3>David Selley acts for Translink in the issuance of $400 million of 3.25% green bonds</h3>
         <p class="small news__card-date">January 29, 2019  •  Joseph Smith</p>
